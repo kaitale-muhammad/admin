24a8250207a02824271b6518350df0a805bf5872
Kaitale Muhammad <137487556+kaitale-muhammad@users.noreply.github.com>
2024-10-17 22:34:04 +0300

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+// Components
+
+export default defineComponent({
+  name: "HomeView",
+
+  components: {},
+});
+</script>
