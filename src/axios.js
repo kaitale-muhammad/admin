45099609e59a43a8da666c2f97de5ce0641c5ad5
Kaitale Muhammad <137487556+kaitale-muhammad@users.noreply.github.com>
2024-11-25 02:16:24 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of Axios
 const api = axios.create({
-  baseURL: "https://backendpsl.up.railway.app/", // https://backendpsl.up.railway.app/
+  baseURL: "https://backendpsl.up.railway.app", // https://backendpsl.up.railway.app/
 });
 
 export default api;
