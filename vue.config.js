61acde6b9399710961609a46dcaade2b0fc6bc9f
Kaitale Muhammad <137487556+kaitale-muhammad@users.noreply.github.com>
2024-11-26 00:52:53 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,21 @@
 
-exports = {
+module.exports = {
   transpileDependencies: true,
+
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://admin2-pt6zosxz.b4a.run', // Backend server URL
+        changeOrigin: true, // Enable CORS proxying
+        pathRewrite: { '^/api': '' }, // Rewrite '/api' to ''
+        secure: true, // If your backend uses HTTPS
+      },
+    },
+  },
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+};
