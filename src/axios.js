a79d7255f0bab588de5fbb4f95512aa06ccf4098
Kaitale Muhammad <kaitalemuhammad1@gmail.com>
2024-12-07 16:31:39 +0300

    upaded login

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of Axios
 const api = axios.create({
-  baseURL: "backend-production-2194.up.railway.app", // https://backendpsl.up.railway.app/
+  baseURL: "https://backend-production-2194.up.railway.app", // https://backendpsl.up.railway.app/
 });
-export const baseUrl = "backend-production-2194.up.railway.app";
+export const baseUrl = "https://backend-production-2194.up.railway.app";
 export default api;
